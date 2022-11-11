934beffe8ac222bc2443f1e109689fd441ed6c27
zwg <zwg.work.mail@gmail.com>
2022-11-11 10:52:17 +0300

    renamed a folder

--- brain_games/scripts/brain_calc.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from brain_games.games.calc_game import main
-
-if __name__ == "__main__":
-    main()
--- brain_games/scripts/brain_games_1.py
+++
@@ -1,9 +0,0 @@
-from brain_games.cli import welcome_user
-
-
-def main():
-    welcome_user()
-
-
-if __name__ == "__main__":
-    main()
